437b52a37060631ae6cce3bd2002db0d7143a3f7
Tyler Fowler <tylerfowler.1337@gmail.com>
2016-04-17 01:01:47 -0500

    Add gulp clean & build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 'use strict';
 
 const gulp  = require('gulp');
+const clean = require('gulp-clean');
 const babel = require('gulp-babel');
 
-gulp.task('default', () =>
-  gulp.src('lib/delve.js')
-  .pipe(babel({ presets: ['es2015'] }))
+gulp.task('clean-dist', () =>
+  gulp.src('dist/', { read: false })
+  .pipe(clean())
+);
+
+gulp.task('default', [ 'clean-dist' ], () =>
+  gulp.src('lib/*.js')
+  .pipe(babel({ presets: [ 'es2015' ] }))
   .pipe(gulp.dest('dist'))
 );
